bfdb04aebc62474246ec8ee191cdac9817c09a7d
Terry Lowery <tl@lowerytech.net>
2021-02-10 21:12:00 -0600

    Messing with middleware

--- mod.ts
+++ mod.ts
@@ -3,6 +3,17 @@ import {Application} from "./dep.ts"
 const app = new Application()
 const PORT = 8000
 
+app.use(async (ctx, next) => {
+    await next();
+    const responseTime = ctx.response.headers.get("X-Response-Time");
+    console.log(`${ctx.request.method} - ${ctx.request.url} - Response: ${ctx.response.status} - Response Time: ${responseTime}`)
+});
+app.use(async(ctx, next) => {
+    const start = Date.now();
+    await next();
+    const delta = Date.now() - start;
+    ctx.response.headers.set("X-Response-Time", `${delta}ms`);
+})
 app.use((ctx) => {
     ctx.response.body = `
         #     #    #     #####     #    
