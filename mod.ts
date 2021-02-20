c9c2203ac6137c8539098cd18f262ce9b93cdf42
Terry Lowery <tl@lowerytech.net>
2021-02-20 12:55:54 -0600

    added serving static content

--- mod.ts
+++ mod.ts
@@ -1,19 +1,31 @@
-import {Application} from "./dep.ts"
+import {Application, send} from "./dep.ts"
 
 const app = new Application()
 const PORT = 8000
 
 app.use(async (ctx, next) => {
+    // access downstream middleware
     await next();
     const responseTime = ctx.response.headers.get("X-Response-Time");
     console.log(`${ctx.request.method} - ${ctx.request.url} - Response: ${ctx.response.status} - Response Time: ${responseTime}`)
 });
 app.use(async(ctx, next) => {
     const start = Date.now();
+    // access downstream middleware
     await next();
     const delta = Date.now() - start;
     ctx.response.headers.set("X-Response-Time", `${delta}ms`);
-})
+});
+app.use(async (ctx) => {
+    const filePath = ctx.request.url.pathname
+    const fileWhitelist = [
+        "/index.html",
+        "/javascripts/script.js",
+        "/stylesheets/style.css",
+        "/images/favicon.png",
+    ]
+    await send(ctx, filePath, {root: `${Deno.cwd()}/public`, })
+});
 app.use((ctx) => {
     ctx.response.body = `
         #     #    #     #####     #    
